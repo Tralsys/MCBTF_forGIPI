db909ba03ef9a4f3f73ac15a9ab43ddb6ad5ecde
Tetsu Otter <tetsu_otter@aol.com>
2018-08-07 18:13:57 +0900

    プロジェクト ファイルを追加します。

---
+++ MCBTF_forGIPI/MCBTF_forGIPI.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MCBTF_forGIPI.ino
+ Created:	2018/08/07 18:13:43
+ Author:	Tetsu Otter
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
